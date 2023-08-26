326ef15cceb2363fd6189b5bf9440a270bca353b
Aamir Reza <amrreza786@gmail.com>
2023-08-26 20:57:44 +0530

    enhanced Switch syntax compared to classic syntax

--- Switch/Example3.java
+++ Switch/Example3.java
@@ -8,21 +8,32 @@ public class Example3 {
         System.out.print("please enter day no: ");
         int day = in.nextInt();
 
+
+//  enchanced  switch expresssion
         switch (day) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Weekdays");
-                break;
-            case 6:
-            case 7:
-                System.out.println("Weekend");
-                break;
-            default:
-                break;
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekdays");
+            case 6, 7 -> System.out.println("Weekend");
+            default -> {
+            }
         }
 
     }
 }
+
+// Classic syntax for this :
+
+//    switch (day) {
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//            System.out.println("Weekdays");
+//            break;
+//            case 6:
+//            case 7:
+//            System.out.println("Weekend");
+//            break;
+//default:
+//        break;
+//        }
