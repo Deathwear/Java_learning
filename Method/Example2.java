26bc5fe7d3b95404605b90bd808de036198b951b
Aamir Reza <amrreza786@gmail.com>
2023-08-30 00:56:26 +0530

    Another example for using method

--- Method/Example2.java
+++ Method/Example2.java
@@ -1,10 +1,31 @@
 package Method;
 
-import java.util.Scanner;
 
 public class Example2 {
+
+    static int logic(int x, int y){
+        int z;
+        if (x>y){
+            z = x + y;
+        }
+        else {
+            z = (x+y) * 5;
+        }
+        return z;
+
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int
+        int a = 5;
+        int b = 7;
+        int c;
+        c = logic(a, b);
+        int a1 = 7;
+        int b1 = 5;
+        int c1;
+        c1 = logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+
     }
 }
