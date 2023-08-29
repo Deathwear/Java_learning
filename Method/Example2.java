33b549808fb64f9a0d3eb5ae950b9f06c7d6bd40
Aamir Reza <amrreza786@gmail.com>
2023-08-30 01:16:29 +0530

    using method with specifying parameter or args.

--- Method/Example2.java
+++ Method/Example2.java
@@ -18,12 +18,10 @@ public class Example2 {
     public static void main(String[] args) {
         int a = 5;
         int b = 7;
-        int c;
-        c = logic(a, b);
+        int c = logic(a, b);
         int a1 = 7;
         int b1 = 5;
-        int c1;
-        c1 = logic(a1, b1);
+        int c1 = logic(a1, b1);
         System.out.println(c);
         System.out.println(c1);
 
