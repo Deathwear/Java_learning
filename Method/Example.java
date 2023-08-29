07a6ba7442819af2956612b3eb612cf74b65f8a5
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-08-29 15:31:14 +0530

    Update README.md
    added complete syllabus for java programming

---
+++ Method/Example.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class Example {
+
+    // this is an example for using methods as many no. of times and proper Syntax
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet(){
+        String greeting = "Hello world...!!";
+        return greeting;
+    }
+}
