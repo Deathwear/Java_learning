b387dceb8896f5f4a495b08bfa98f41654574507
Aamir Reza <amrreza786@gmail.com>
2023-08-30 20:19:42 +0530

    added instructions

--- Method/Varargs.java
+++ Method/Varargs.java
@@ -1,7 +1,7 @@
 package Method;
 
 import java.util.Arrays;
-
+// here we don't know the no. exvactly how many arguements are we going to take that's why we use Varargs
 public class Varargs {
     public static void main(String[] args) {
         fun(2, 3, 45, 46, 23, 78, 85);
