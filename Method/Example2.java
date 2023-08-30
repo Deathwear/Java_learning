2c4f7816428083a55ab268982057e10971d7b49d
Aamir Reza <amrreza786@gmail.com>
2023-08-30 18:56:53 +0530

    Method invocation using Object creation

--- Method/Example2.java
+++ Method/Example2.java
@@ -15,10 +15,15 @@ public class Example2 {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        // Method invocation using Object creation 
+        // Example2 obj = new Example2();
+        //c = obj.logic(a,b);  
+        // this wil execute when we don't provide static in method
         int a = 5;
         int b = 7;
         int c = logic(a, b);
+        
         int a1 = 7;
         int b1 = 5;
         int c1 = logic(a1, b1);
