37b197c08c85b76140bf081d4caaf937744b9c86
Aamir Reza <amrreza786@gmail.com>
2023-08-30 20:29:15 +0530

    added another example for mix Varargs

--- Method/Varargs.java
+++ Method/Varargs.java
@@ -5,10 +5,15 @@ import java.util.Arrays;
 public class Varargs {
     public static void main(String[] args) {
         fun(2, 3, 45, 46, 23, 78, 85);
+        multiple(1, 5, "Aamir", "Reza", "args");
     }
 
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));
+
+    }
+    static void multiple(int a, int b, String ...v){
+        System.out.println();
 
     }
 }
