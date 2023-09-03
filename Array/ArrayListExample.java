a36e694be0ef9ae13514b3f7c398578f639edff9
Aamir Reza <amrreza786@gmail.com>
2023-09-03 20:15:47 +0530

    Example when Column is Not Fixed

---
+++ Array/ArrayListExample.java
@@ -0,0 +1,10 @@
+package Array;
+import java.util.ArrayList;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // Syntax
+        ArrayList<Integer> list = new ArrayList<>();
+        
+
+    }
+}
