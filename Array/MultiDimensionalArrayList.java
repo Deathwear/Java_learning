7885da15cb0564a2063601928f8c8927979d1b38
Aamir Reza <amrreza786@gmail.com>
2023-09-05 01:09:51 +0530

    Multi Dimensional ArrayList example

---
+++ Array/MultiDimensionalArrayList.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiDimensionalArrayList {
+    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        //initialisation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add elements
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(new Scanner(System.in).nextInt());
+            }
+        }
+
+        System.out.println(list);
+
+    }
+}
