bfce47e9a3e3ecd952675383c414e1bb2d5f6a76
Aamir Reza <amrreza786@gmail.com>
2023-09-15 21:14:02 +0530

    Searching in String

--- Linear_Search/SearchInStrings.java
+++ Linear_Search/SearchInStrings.java
@@ -2,6 +2,23 @@ package Linear_Search;
 
 public class SearchInStrings {
     public static void main(String[] args) {
-
+        String name = "Aamir";
+        char target = 'm';
+        System.out.println(name, target);
     }
+
+    static boolean  search(String str, char target){
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 }
