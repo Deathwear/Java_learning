c4b3962c842503ad12ae63b8dbcc75cef0e7f1a1
Aamir Reza <amrreza786@gmail.com>
2023-09-18 17:30:31 +0530

    updated

--- Linear_Search/SearchInStrings.java
+++ Linear_Search/SearchInStrings.java
@@ -4,11 +4,11 @@ public class SearchInStrings {
     public static void main(String[] args) {
         String name = "Aamir";
         char target = 'm';
-        System.out.println(name, target);
+        System.out.println(search(name,target));
     }
 
     static boolean  search(String str, char target){
-        if (str.length() == 0) {
+        if (str.length() == 0) {   //this can also be replaced by str.isEmpty()
             return false;
         }
         for (int i = 0; i < str.length(); i++) {
@@ -18,7 +18,7 @@ public class SearchInStrings {
         }
         return false;
     }
-
-
-
 }
+//return true if target element is present in the String name.
+// otherwise prints false.
+ 
