eeae8bd6c135d784255612c32560f3e0489702c7
Aamir Reza <amrreza786@gmail.com>
2023-09-20 19:18:36 +0530

    updated

--- Linear_Search/SearchInStrings.java
+++ Linear_Search/SearchInStrings.java
@@ -1,10 +1,27 @@
 package Linear_Search;
 
+import java.util.Arrays;
+
 public class SearchInStrings {
     public static void main(String[] args) {
         String name = "Aamir";
         char target = 'm';
-        System.out.println(search(name,target));
+        // System.out.println(search(name,target));
+
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+        static boolean search2(String str, char target){
+            if (str.length() == 0) {
+                return false;
+            }
+            for (char ch : str.toCharArray()) {
+                if(ch == target){
+                    return true;
+                }
+            }
+            return false;
+
     }
 
     static boolean  search(String str, char target){
