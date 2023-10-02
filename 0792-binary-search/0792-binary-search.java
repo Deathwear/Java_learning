4d7fcec4b202731c2e512e4665c0697849d938ff
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-10-02 12:20:32 +0530

    Time: 0 ms (100%), Space: 44.2 MB (92.07%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // declaring all pointers
+        int start = 0;
+        int end = nums.length-1;
+        
+
+        //uisng while loop for starting iteration 
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target > nums[mid]){  // given ascending order
+                start = mid + 1;
+            }else if(target < nums[mid]){
+                end = mid -1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
