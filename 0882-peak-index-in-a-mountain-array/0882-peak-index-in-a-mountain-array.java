ea98242b17537e204fbdd7567ba826847f996326
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-10-04 20:55:54 +0530

    Time: 0 ms (100%), Space: 56 MB (39.61%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,18 @@
+// Using same approach of binary search 
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){  // loop will break when start = end (this is the peak point)
+            int mid = start + (end - start)/2;
+            if(arr[mid]<arr[mid+1]){
+                start = mid +1;
+            }else {  // when arr[mid] > arr[mid+1]
+                end = mid;
+            }
+        }
+        return end;   // we can also return  start here they both are equal at this point.
+    }
+    
+}
