3897cfa92c8f5c4c92bc820d0912b15a8a70707a
Aamir Reza <amrreza786@gmail.com>
2023-10-21 17:44:00 +0530

    Matrix is sorted row & column wise both

--- Binary_Search/RowColMatrix.java
+++ Binary_Search/RowColMatrix.java
@@ -1,5 +1,20 @@
 package Binary_Search;
 
+// Finding a target in 2-D array. (Matrix is sorted in row wise & column wise)
+/* WORKING:
+            First make a search method
+            in this search method, we pass the array and target and start searching for it
+            in this method, we initialized row at 0th row and column at last column
+            then by taking the last element of each row  and comparing it with the target (bcz it is only sorted in a row)
+            if that element == target; (then we found our target)
+            elseif, target > that element; then we have to look for the greater part of the array (means ignoring the smaller part)
+            we have to do row++ (it will present in that row)
+            else (i.e. target<that element) we do col-- (bcz matrix is also sorted column wise) means that target is not present in that column
+
+
+            that's how we are decreasing our search area every time by column and row until we find the target.
+ */
+
 import java.util.Arrays;
 
 public class RowColMatrix {
