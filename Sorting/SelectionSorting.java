156101a46ff8febaa2db1b72bced28f8e4c60c5c
Aamir Reza <amrreza786@gmail.com>
2023-10-24 20:32:12 +0530

    updated

--- Sorting/SelectionSorting.java
+++ Sorting/SelectionSorting.java
@@ -10,14 +10,17 @@ public class SelectionSorting {
     }
 
     public static void selectionSorting(int[] arr){
+        // here we have decided our search area,
+        // which will get reduced after every iteration of maxValue to the last index.
         for (int i = 0; i < arr.length; i++) {
-            //find the maximum item in the remaining array and swap with correct index.
+            //find the maximum item in the remaining array and swap with the correct index.
             int last = arr.length - i- 1;  // last index where to place the largest element for that
-            int maxIndex = getMaxIndex(arr, 0, last);
-            swap(arr, maxIndex, last);
+            int maxIndex = getMaxIndex(arr, 0, last);  // here last is the end value
+            swap(arr, maxIndex, last);    // we get the max value in our searching area, and we need to replace it to the last position, therefore we are needed to swap the maxIndex value with the last position of Scope area.
         }
     }
 
+    // Function for swapping the maxValue element(i.e., int first) with the second value element (i.e., the last position of our search area)
     public static void swap(int[] arr, int first, int second){
         int temp = arr[first];
         arr[first] = arr[second];
@@ -34,3 +37,9 @@ public class SelectionSorting {
         return max;
     }
 }
+
+/*
+In selection sort, we are needed to select the highest value of an element in our array and put it on the last index value
+and reduce our search area and then repeat the process till the whole array is sorted.
+
+ */
