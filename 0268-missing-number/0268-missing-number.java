274f02abb88f48ac977a49a1d1302d8445e26cd0
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-10-26 16:12:10 +0530

    Time: 1 ms (40.88%), Space: 43.9 MB (61.73%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+            if (nums[i]< nums.length && nums[i] != nums[nums[i]] ) { // if the value of element is not according to value of current index
+                swap(nums, nums[i], i ); // then swap those elements.
+            }else{ // if the element has not any correct by now therefore we are needed to skip that element and sort other elements all the elements  will be sorted except that element.
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for(int index = 0; index < nums.length; index++){
+            if(index != nums[index]){
+                return index;
+            }
+        }
+        return nums.length; // here it means that all the element are present at right index therefore the last element is missing that's why we are needed to return last element'
+    }
+    // Swapping method is used for swapping the elements to its correct postion 
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
