d2c6ebd52ae75cb613bed06fefbf5eac2352f802
Aamir Reza <amrreza786@gmail.com>
2023-11-01 19:23:09 +0530

    update

--- ArrayList/MultiDimensionalArrayList.java
+++ ArrayList/MultiDimensionalArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MultiDimensionalArrayList {
-    
+
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
