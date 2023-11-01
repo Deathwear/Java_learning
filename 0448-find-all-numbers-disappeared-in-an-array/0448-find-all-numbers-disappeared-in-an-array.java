d3434a3852d047b45718d4b326c64f79ea607657
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-11-01 20:02:04 +0530

    Time: 5 ms (85.77%), Space: 52.9 MB (88.58%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i =0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if( nums[i]<= nums.length && nums[i] != nums[correct]){ 
+                swap(nums, i, correct);
+            }else{
+                i++;
+            }
+        }
+        // now the array is cyclic sorted (it also works for duplicate values by ignoring them)
+        // now checking for all missign nums in the cyclic sorted array
+        ArrayList<Integer> ans = new ArrayList<>();// creatign array list with not fixed length to store values of missing numbers (bcz we don't know the values of missing numbers')
+        for(int index =0; index< nums.length; index++){
+            if(nums[index] != index +1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+
+    public void swap(int[]nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
