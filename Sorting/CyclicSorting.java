c001218c37192d52d6fb7c889b9ac9705307e20b
Aamir Reza <amrreza786@gmail.com>
2023-11-01 19:00:51 +0530

    Update 0268-missing-number.java

--- Sorting/CyclicSorting.java
+++ Sorting/CyclicSorting.java
@@ -12,7 +12,7 @@ public class CyclicSorting {
     static void cyclicSorting(int[] arr){
         int i = 0;
         while (i < arr.length){
-            int correct = arr[i] - 1;  // it is stating that correct index of the element is its (value - 1)
+            int correct = arr[i] - 1;  // it is stating that correct index of the element is its (value - 1) (When Range starts from 1 to n)
             if (arr[i] != arr[correct]) { // if the value of element is not according to value of current index
                 swap(arr, i, correct); // then swap those elements.
             }else{
