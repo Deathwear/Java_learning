daebb61285771bd4df809a15a4517aa286b9adca
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-11-02 17:38:03 +0530

    Time: 5 ms (59.02%), Space: 55.9 MB (91.76%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int i =0;
+        // performing cyclic sort
+        while(i<arr.length){
+            if(arr[i] != i+1){
+                int correct = arr[i] -1;
+                if(arr[i] != arr[correct]){
+                    swap(arr, i, correct);
+                }else{  //this will only be executed when when arr we have isnot at their correct index but the element is same as the element which is at its correct index (means it is duplicate elememt)
+                    return arr[i];
+                }
+            }else{
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    public void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
