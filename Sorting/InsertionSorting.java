da1959d9e19f46a175590804052b9be1e9c2e148
Aamir Reza <amrreza786@gmail.com>
2023-11-08 19:49:41 +0530

    added notes

--- Sorting/InsertionSorting.java
+++ Sorting/InsertionSorting.java
@@ -14,7 +14,7 @@ public class InsertionSorting {
     public static void insertionSorting(int[] arr){
         for (int i = 0; i < arr.length-1; i++) {  // (i will run till length-2)
             // j is starting after ith position, moving backwards by comparing and replacing them. if it is bigger value then it breaks because previous elements are already sorted by it (it has started from the beginning and already checked it) i.e., started from comparing zero at the very beginning.
-            for (int j = i+1; j >0; j--) {
+            for (int j = i+1; j >0; j--) {   // j will run till 0th index i.e., j>0 will be comapred with j at 0th index at the last
                 if (arr[j] < arr[j-1]) {
                     swap(arr, j , j-1);
                 }else{  // You can debug and check for this part.
