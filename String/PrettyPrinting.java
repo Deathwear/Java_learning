e3c5e0cc8d598eecd81693180ea84ee35f5c5fa6
Aamir Reza <amrreza786@gmail.com>
2023-11-09 20:17:39 +0530

    uses of PlaceHolder

--- String/PrettyPrinting.java
+++ String/PrettyPrinting.java
@@ -3,11 +3,11 @@ package String;
 public class PrettyPrinting {
     public static void main(String[] args) {
         float a = 453.1274f;
-//        System.out.printf("Formatted number is %.2f", a);
+//        System.out.printf("Formatted number is %.2f", a); // prints till 2 decimal digits.
 
-//        System.out.printf("Pie: %.3f", Math.PI);
+//        System.out.printf("Pie: %.3f", Math.PI);  // it will print the decimal value up to 3-digits.
 
-        System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");  // here %s is used as placeholder for prettyprinting
+        System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");  // here %s is used as placeholder for pretty-printing
     }
 }
 
