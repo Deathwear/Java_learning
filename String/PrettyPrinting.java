c770ed6a64d61729f426d5c064076c312631ab5d
Aamir Reza <amrreza786@gmail.com>
2023-11-09 20:09:32 +0530

    uses of PlaceHolder

---
+++ String/PrettyPrinting.java
@@ -0,0 +1,15 @@
+package String;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+//        System.out.printf("Formatted number is %.2f", a);
+
+//        System.out.printf("Pie: %.3f", Math.PI);
+
+        System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");  // here %s is used as placeholder for prettyprinting
+    }
+}
+
+
+// check Placeholder_list.txt for using these kinds for PrettyPrinting
