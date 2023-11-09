986230f37d96ffabe8c738ca026dcea87d59a1f3
Aamir Reza <amrreza786@gmail.com>
2023-11-09 19:38:46 +0530

    string basic concept

---
+++ String/Main.java
@@ -0,0 +1,16 @@
+package String;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Aamir Reza";  // strings are immutable
+//        System.out.println(name);
+
+        String a = "Aamir";
+        System.out.println(a);
+        a = "Aamir";
+        System.out.println(a);   // here pooling concept is used therefore, both a will points towards same object in the heap memory in the pool.
+    }
+}
+// if we decide to change the value of 'a' then a will points towards another object and Aamir will go to garbage collection.
