5d3a1c6fad0b19c915bf87f1bcec9671eab5e5c7
Aamir Reza <amrreza786@gmail.com>
2023-11-13 17:55:22 +0530

    updated

--- String/Performance.java
+++ String/Performance.java
@@ -7,10 +7,14 @@ public class Performance {
             char ch = (char)('a' + i);
             System.out.print(ch + " ");
             series = series + ch;  // series += ch
+
+            // in the series it is copying the earlier series and appending a value into it and then printing it (every time it is creating a new string bcz strings are immutable) so much of memory/space wastage.
+            // time complexity of O(n^2)
         }
 
-        System.out.println(series);
+        System.out.println(series);   // series will be printed only once when the for loop is terminated.
     }
 }
 
 // Timestamp : 1:14:00
+// it would be great if there is a datatype which allow modification in itself. (no new object is needed to create) - StringBuilder
