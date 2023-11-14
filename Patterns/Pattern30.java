6173c810d2ae20713a08056c50fddf607cbe2bc4
Aamir Reza <amrreza786@gmail.com>
2023-11-14 21:05:37 +0530

    solved ques30 of Patterns.md

--- Patterns/Pattern30.java
+++ Patterns/Pattern30.java
@@ -9,13 +9,13 @@ public class Pattern30 {
 
             // only spaces are remaining
             for (int spaces = 0; spaces < n-row ; spaces++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int col = row; col >= 1 ; col--) {
-                System.out.print(col);
+                System.out.print(col + " ");
             }
             for (int col = 2; col <= row; col++) {
-                System.out.print(col);
+                System.out.print(col+ " ");
             }
             System.out.println();
         }
