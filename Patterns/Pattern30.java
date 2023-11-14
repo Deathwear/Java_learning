4b373856f7a17c0cdb03262f0474d4521706768f
Aamir Reza <amrreza786@gmail.com>
2023-11-14 21:04:19 +0530

    solved ques30 of Patterns.md

---
+++ Patterns/Pattern30.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Pattern30 {
+    public static void main(String[] args) {
+        patter30(5);
+    }
+    static void patter30(int n){
+        for (int row = 0; row <= n; row++) {
+
+            // only spaces are remaining
+            for (int spaces = 0; spaces < n-row ; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = row; col >= 1 ; col--) {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
