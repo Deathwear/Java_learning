9897a518ce837217c0bd522d98aa97d0c4a737db
Aamir Reza <amrreza786@gmail.com>
2023-11-14 19:43:48 +0530

    solved ques5 of Patterns.md

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+import javax.crypto.spec.PSource;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        System.out.println("The pattern 2 is printed like this: ");
+        pattern1(5);
+    }
+
+    static void pattern1(int n){
+        for (int row = 1; row <= 2*n-1; row++) {
+            int totalColsInRow = row >n ? 2 *n - row: row;  //if columns are greater than n then total columns will be 2n-row else they will be like only equal to row like in ques 2.
+            for (int col = 1; col <= totalColsInRow; col++) {
+                System.out.print("*");
+            }
+            //when one row is printed, we are needed to add a newline.
+            System.out.println();
+        }
+    }
+}
