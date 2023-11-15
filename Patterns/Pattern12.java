2607155c04eef80bbdb87e9ef3ff4ac9ed28a0e7
Aamir Reza <amrreza786@gmail.com>
2023-11-15 18:55:42 +0530

    solved ques12 of Patterns.md

--- Patterns/Pattern12.java
+++ Patterns/Pattern12.java
@@ -7,11 +7,11 @@ public class Pattern12 {
 
     static void pattern5(int n){
         for (int row = 1; row <= 2*n; row++) {
-            int totalColsInRow = row > n ? 2 *n - row+1: row;
+            int totalColsInRow = row > n ? 2 *n - row+1: row;  // If-Else notation.
             for (int col = 0; col < totalColsInRow; col++) {
                 System.out.print(" ");
             }
-            int totalStartsneeded = row > n ? row-n : n-row+1;
+            int totalStartsneeded = row > n ? row-n : n-row+1;  // If-Else notation.
             for (int col = 0; col < totalStartsneeded; col++) {
                 System.out.print("* ");
             }
