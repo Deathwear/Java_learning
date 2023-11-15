656018df0d167941326842d10af4578cd7ac53af
Aamir Reza <amrreza786@gmail.com>
2023-11-15 18:50:53 +0530

    solved ques12 of Patterns.md

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        pattern5(5);
+    }
+
+    static void pattern5(int n){
+        for (int row = 1; row <= 2*n; row++) {
+            int totalColsInRow = row > n ? 2 *n - row+1: row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print(" ");
+            }
+            int totalStartsneeded = row > n ? row-n : n-row+1;
+            for (int col = 0; col < totalStartsneeded; col++) {
+                System.out.print("* ");
+            }
+            //when one row is printed, we are needed to add a newline.
+            System.out.println();
+        }
+    }
+}
