f6320da036efa1478455529e8f28c8b885164130
Aamir Reza <amrreza786@gmail.com>
2023-11-16 19:23:55 +0530

    Basic function for understanding working of recusion (not an example of recursion)

---
+++ Recursion/NumberExample.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class NumberExample {
+    public static void main(String[] args) {
+        // write a funtion that takes in a number and prints it
+        // print first 5 numbers: 1 2 3 4 5
+        print(1);
+    }
+    static void print(int n){
+        System.out.println(n);
+        print1(2);
+    }
+    static void print1(int n){
+        System.out.println(n);
+        print2(3);
+    }
+    static void print2(int n){
+        System.out.println(n);
+        print3(4);
+    }
+    static void print3(int n){
+        System.out.println(n);
+        print4(5);
+    }
+    static void print4(int n){
+        System.out.println(n);
+    }
+}
