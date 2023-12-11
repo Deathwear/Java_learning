f1f5738d11bcab53c76e27937c5e834063de31b7
Aamir Reza <amrreza786@gmail.com>
2023-12-11 17:50:49 +0530

    Checking even or odd using Bitwise Manipulation

--- Maths_for_DSA/OddEven.java
+++ Maths_for_DSA/OddEven.java
@@ -5,7 +5,7 @@ package Maths_for_DSA;
 public class OddEven {
     public static void main(String[] args) {
         int n = 67;
-        System.out.println(isOdd(n));
+        System.out.print(isOdd(n));
     }
 
     private static boolean isOdd(int n){
