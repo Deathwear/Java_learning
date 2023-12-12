f356d7158c1bb3b7419844b1fa305a46ebc9ded1
Aamir Reza <amrreza786@gmail.com>
2023-12-12 17:23:51 +0530

    amazon question on bitwise manipulation

--- Maths_for_DSA/MagicNumber.java
+++ Maths_for_DSA/MagicNumber.java
@@ -4,15 +4,15 @@ package Maths_for_DSA;
 
 public class MagicNumber {
     public static void main(String[] args) {
-        int n =6;
+        int n =6;  // Binary representation of 6 is 110
         int ans = 0;
         int base = 5;
 
         while (n>0){
-            int last = n & 1;  // this will give the last digit in binary representation
-            n = n >> 1;  // right shifting the binary nubmer to get the next last digit
+            int last = n & 1;  // this will give the last digit in binary representation i.e., 0 for 1st loop
+            n = n >> 1;  // right shifting the binary nubmer to get the next last digit (in first loop 110 will become 11 i.e., 3 in decimal representation)
             ans += last * base;
-            base = base * 5;
+            base = base * 5;  // every time base will be multiplied by 5 as the condition of the magic number.
         }
         System.out.println(ans);
     }
