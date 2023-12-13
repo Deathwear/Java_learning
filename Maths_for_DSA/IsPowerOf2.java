11d903263dc356c3bf0adffa929c60814e95d7a7
Aamir Reza <amrreza786@gmail.com>
2023-12-13 18:34:14 +0530

    checking if the no. is power of two or not

--- Maths_for_DSA/IsPowerOf2.java
+++ Maths_for_DSA/IsPowerOf2.java
@@ -2,7 +2,7 @@ package Maths_for_DSA;
 
 public class IsPowerOf2 {
     public static void main(String[] args) {
-        int n = 16;
+        int n = 16;  // Note: fix for n = 0
         boolean ans = (n & (n-1)) == 0;    // binary representation of 2's power number has only one 1 so so doing operation with all the other zero with 1 we get all 0
         System.out.println(ans);
     }
