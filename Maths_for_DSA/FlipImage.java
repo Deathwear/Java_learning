b2cf9a55a9cc10999daee063f62dd97f3c2237a0
Aamir Reza <amrreza786@gmail.com>
2023-12-14 18:47:26 +0530

    leetcode sollution using xor for flip and reverse of 2d-array

--- Maths_for_DSA/FlipImage.java
+++ Maths_for_DSA/FlipImage.java
@@ -5,13 +5,13 @@ public class FlipImage {
     }
 
     public int[][] flipAndInvertImage(int[][] image) {
-        for (int[] row: image){
+        for (int[] row : image){
             // revert the elements of the 2d array.
             for (int i = 0; i < (image[0].length + 1) / 2; i++) {
                 // swap
                 int temp = row[i]  ^ 1;  // XOR with 1 to get the opposite value of the elements.
-                row[i] = row[image[0].length-i -1];
-                row[image[0].length-i -1] = temp;
+                row[i] = row[image[0].length- i -1] ^ 1; // doing XOR for flipping the value.
+                row[image[0].length- i -1] = temp;
             }
         }
         return image;
