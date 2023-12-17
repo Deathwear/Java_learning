b823c2b793998fe4dc0a0596b313e0e73ab7931d
Aamir Reza <amrreza786@gmail.com>
2023-12-17 20:21:53 +0530

    added program for finding LCM of two number using LCM= fgd where d = HCF(gcd)

--- Maths_for_DSA/GCD_LCM.java
+++ Maths_for_DSA/GCD_LCM.java
@@ -2,7 +2,8 @@ package Maths_for_DSA;
 
 public class GCD_LCM {
     public static void main(String[] args) {
-        System.out.println(gcd(224, 105));
+        System.out.println("HCF of 4&14 is: " + gcd(4, 14));
+        System.out.println("LCM of 4&14 is: " + lcm(4, 14));
     }
 
     static int gcd(int a, int b){
@@ -10,5 +11,9 @@ public class GCD_LCM {
             return b;   // when a%b remainder becomes zero, the recursion breaks, and we will get b as ans.
         }
         return gcd(b%a, a);  // b%a will give a remainder then we will do the gcd of remainder and a again and again until we get 0 as a remainder or a.
+    }
+
+    static int lcm(int a, int b){
+        return a * b / gcd(a,b);
     }
 }
