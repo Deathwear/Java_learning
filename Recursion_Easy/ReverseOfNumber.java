f4c198923504bc36c43e38e1160d5265a70bcdf1
Aamir Reza <amrreza786@gmail.com>
2023-12-19 20:49:59 +0530

    using another recursive funtion as helper for passing the arguements to find ReverseOfNumber

--- Recursion_Easy/ReverseOfNumber.java
+++ Recursion_Easy/ReverseOfNumber.java
@@ -6,10 +6,11 @@ public class ReverseOfNumber {
         System.out.println();
         reverse2(1884026);
         System.out.println(sum);
+        System.out.println(reverse3(1234));
     }
 
     static int sum = 0;
-    static void reverse2(int n){
+    static void reverse2(int n){   // we are taking extra variable outside the function (cheating/not pure recursion)
         if (n == 0){
             return;
         }
@@ -24,5 +25,20 @@ public class ReverseOfNumber {
         }
         System.out.print(n%10);   // This is cheating all numbers are printed separately.
         reverse1(n/10);
+    }
+
+    static int reverse3(int n){
+        // sometimes you need some addition variables in the arguments
+        // in that case, make another function.
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits){
+        if (n%10 == n){
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int)Math.pow(10, digits-1) + helper(n/10, digits-1);
     }
 }
