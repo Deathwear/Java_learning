23a6e45d0eb61899a09631dddac2e8af3ea19a87
Aamir Reza <amrreza786@gmail.com>
2023-12-19 17:27:48 +0530

    difference between n-- & --n

---
+++ Recursion_Easy/Concept.java
@@ -0,0 +1,16 @@
+package Recursion_Easy;
+
+public class Concept {
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    static void fun(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.println(n);
+//        fun(n--);
+        fun(--n);
+    }
+}
