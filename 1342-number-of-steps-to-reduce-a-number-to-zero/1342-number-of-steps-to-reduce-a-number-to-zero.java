3b88f4a0c19e0d8bea8b4e16fa2e11ff764d800a
Aamir Reza <101553320+Deathwear@users.noreply.github.com>
2023-12-21 00:06:03 +0530

    Time: 0 ms (100%), Space: 40.2 MB (6.15%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int numberOfSteps(int num){
+        return helper(num, 0);
+    }
+
+    private static int helper(int num, int steps){
+        if (num == 0){
+            return steps;
+        }
+
+        if (num%2 == 0){
+            return helper(num/2, steps+1);
+        }
+        return helper(num-1, steps+1);
+    }
+}
