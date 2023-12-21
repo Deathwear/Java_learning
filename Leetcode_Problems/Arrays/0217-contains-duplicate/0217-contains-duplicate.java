25c61026dbd819a235eeaabcbd47cd284bbeb893
Aamir Reza <amrreza786@gmail.com>
2023-12-21 21:15:21 +0530

    updated leetcode Solution0217 contains-duplicate.java

--- Leetcode_Problems/Arrays/0217-contains-duplicate/0217-contains-duplicate.java
+++ Leetcode_Problems/Arrays/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,4 +1,6 @@
-class Solution {
+import java.util.Arrays;
+
+class Solution0217 {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i=0; i<nums.length-1; i++){
