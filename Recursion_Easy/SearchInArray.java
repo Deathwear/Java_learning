74fd4ff2fe692f16ec322124241538b2d1a56b87
Aamir Reza <amrreza786@gmail.com>
2023-12-23 18:21:11 +0530

    added method to find all the occurrences of target in an ArrayList by creating inside and outside the body of function (both methods)

--- Recursion_Easy/SearchInArray.java
+++ Recursion_Easy/SearchInArray.java
@@ -9,8 +9,15 @@ public class SearchInArray {
         System.out.println("Is our target present in the array => " + searchIndex(arr, 4, 0));
         System.out.println("The index value of our target is: " + search(arr, 4, 0));
         System.out.println("The index value of our target is: "+searchFromLast(arr, 4, arr.length-1));
+
+        // This is about creating ArrayList outside the function and calling it inside the function.
         searchAllIndex(arr, 4, 0);
-        System.out.println("Index value of all occurrences are: " + list);
+        System.out.println("Index value of all occurrences are: " + list  + " ArrayList is created outside here.");
+
+
+        // this is about creating arraylist inside the body of the function
+        ArrayList<Integer> ans = searchAllIndexlist(arr, 4, 0, new ArrayList<>()); // new ArrayList is initialized here
+        System.out.println(ans + " ArrayList is created inside the body of the function");
     }
 
     static int search(int[] arr, int target, int index){
@@ -51,5 +58,15 @@ public class SearchInArray {
             list.add(index);
         }
         searchAllIndex(arr, target, index + 1);
+    }
+
+    static ArrayList<Integer> searchAllIndexlist(int[] arr, int target, int index,  ArrayList<Integer> list){
+        if (index == arr.length){  // the index value reaches its end then the whole list value is returned to its previous own searchAllIndexlist function
+            return list ;
+        }
+        if (arr[index] == target){
+            list.add(index);   // it will add the values to the ArrayList but doen't return it
+        }
+        return searchAllIndexlist(arr, target, index + 1, list);  // this will return its whole value to its previous own function call till all the memory in the stack.
     }
 }
