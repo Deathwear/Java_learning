4d403f78763455f9afb9537202caf333a99bc61e
Aamir Reza <amrreza786@gmail.com>
2023-12-25 18:49:30 +0530

    Doing pattern questions using recursion

---
+++ Recursion/Pattern.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+public class Pattern {
+    public static void main(String[] args) {
+        triangle(4,0);
+        triangle2(4,0);
+    }
+    static void triangle (int r, int c){
+        if (r==0){
+            return;
+        }
+        if (c<r){
+            System.out.print(" * ");
+            triangle(r, c+1);
+        }else {
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
+
+    static void triangle2 (int r, int c){
+        if (r==0){
+            return;
+        }
+        if (c<r){
+            triangle2(r, c+1);
+            System.out.print(" * ");   // prints only when they are removing from stack.
+        }else {
+            triangle2(r-1, 0);
+            System.out.println();
+        }
+    }
+}
