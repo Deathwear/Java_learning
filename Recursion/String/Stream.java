a079a41665c810d0050d2ae6b5cc601c3f800649
Aamir Reza <amrreza786@gmail.com>
2024-01-06 20:15:34 +0530

    skipping characters of string using recursion

--- Recursion/String/Stream.java
+++ Recursion/String/Stream.java
@@ -3,8 +3,10 @@ package Recursion.String;
 public class Stream {
     public static void main(String[] args) {
         skip("", "baccad");
+        System.out.println(skip_Method2("baccdah"));
     }
 
+    // in this method ans is used in the arguements.
     static void skip(String p, String up ){ // p = processed, up = unprocessed.
         // base condition.
         if (up.isEmpty()){
@@ -17,6 +19,23 @@ public class Stream {
             skip(p, up.substring(1));
         }else { // else add that char into the processed string.
             skip(p + ch, up.substring(1));
+        }
+    }
+
+
+    // ans is used in the body of the recursive call.
+    static String skip_Method2(String up ){ // p = processed, up = unprocessed.
+        // base condition.
+        if (up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (ch == 'a'){  // if the char at 0th index is a then we don't need to add it in our processed string.
+            return skip_Method2(up.substring(1));
+        }else { // else add that char into the processed string.
+            return ch + skip_Method2(up.substring(1));
         }
     }
 }
