60a4bda112c71c0e5067938429a20a6ac2fbe549
Aamir Reza <amrreza786@gmail.com>
2024-01-08 20:27:31 +0530

    returning the subsets of a set using recursion when return type is ArrayList

--- Recursion/SubSequence.java
+++ Recursion/SubSequence.java
@@ -1,8 +1,11 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class SubSequence {
     public static void main(String[] args) {
         subseq("", "abc");
+        System.out.println(subseqArray("", "abc"));
     }
 
     static void subseq(String p, String up){
@@ -15,5 +18,23 @@ public class SubSequence {
 
         subseq(p + ch, up.substring(1));
         subseq(p, up.substring(1));
+    }
+
+    // if a return type is array.
+    static ArrayList<String> subseqArray(String p, String up){
+        if (up.isEmpty()){ // Base condition.
+            // create a new arraylist and add all processed values in it.
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subseqArray(p + ch, up.substring(1));
+        ArrayList<String> right = subseqArray(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
