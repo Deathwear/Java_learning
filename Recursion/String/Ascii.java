fc8699031773720ac9c34db4ec2f2ae4b24413f8
Aamir Reza <amrreza786@gmail.com>
2024-01-08 20:39:56 +0530

    code to print the ascii value of that character

---
+++ Recursion/String/Ascii.java
@@ -0,0 +1,9 @@
+package Recursion.String;
+
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'a';
+        // printing the ascii value of that character.
+        System.out.println(ch+0);
+    }
+}
