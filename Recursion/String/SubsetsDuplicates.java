bf9abadd56d48324efdbfb925d4afb726e6e2c7e
Aamir Reza <amrreza786@gmail.com>
2024-01-21 17:35:38 +0530

    Printing subsets when there is a duplicate

---
+++ Recursion/String/SubsetsDuplicates.java
@@ -0,0 +1,39 @@
+package Recursion.String;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> ans = subsetsDuplicates(arr);
+        for (List<Integer> list : ans){
+            System.out.println(list);
+        }
+    }
+
+    static List<List<Integer>> subsetsDuplicates(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start =0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element is same, s = e+1
+            if (i > 0 && arr[i] == arr[i-1]) {
+                start = end +1;  // if there is a duplicate element then it will be ignored bcz start will starts from end+1 (ignoring the previous arraylist)
+            }
+            end = outer.size()-1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
