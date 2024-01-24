cf35e30d751f9bc064615d16ea029b6c08beb688
Aamir Reza <amrreza786@gmail.com>
2024-01-24 19:17:00 +0530

    Possible combination of a dice to get the target Arraylist as returnType

--- Recursion/Practice/DiceProblem.java
+++ Recursion/Practice/DiceProblem.java
@@ -3,9 +3,12 @@
 
 package Recursion.Practice;
 
+import java.util.ArrayList;
+
 public class DiceProblem {
     public static void main(String[] args) {
         dice("", 4);
+        System.out.println(diceList("", 4));
     }
 
     static void dice(String p, int target){
@@ -18,5 +21,21 @@ public class DiceProblem {
             dice(p+i, target-i);
 
         }
+    }
+
+    // Return type as an ArrayList
+    static ArrayList<String> diceList(String p, int target){
+        if (target == 0) {
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= 6 && i<= target; i++) {
+            list.addAll(diceList(p+i, target-i));  // processed me jitna judega utna target me minus bhi hoga
+        }
+        return list;
     }
 }
