ae4fabaa5a718a277721616bd00a7cd903a43cb9
Aamir Reza <amrreza786@gmail.com>
2024-01-25 18:36:06 +0530

    possible no. of paths in a maze using only right&down

---
+++ Backtracking/MazeOnlyRight_down.java
@@ -0,0 +1,22 @@
+// possible ways of moving from one point to another using oly right and down
+
+package Backtracking;
+
+public class MazeOnlyRight_down {
+    public static void main(String[] args) {
+        System.out.println(count(3, 3));
+    }
+
+    static int count(int row, int col){
+        if (row == 1 || col == 1) {
+            return 1;
+        }
+
+        int right = count(row-1, col);  // if man moves right then row will be decreased
+        int down = count(row, col-1);   // if man moves down then col will be reduced
+
+        return right + down;
+    }
+}
+
+// We will use dynamic programming concept in the future for this type of questions.
